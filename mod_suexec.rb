046216303a6c49f63c4a646dc04048d00540caed
Alan Ivey <alanivey@gmail.com>
2013-11-03 13:41:06 -0500

    Remove 10.8-only logic

--- mod_suexec.rb
+++ mod_suexec.rb
@@ -5,43 +5,22 @@ class ModSuexec < Formula
   homepage 'http://httpd.apache.org/docs/current/suexec.html'
   sha1 '766cd0843050a8dfb781e48b976f3ba6ebcf8696'
 
+  depends_on 'libtool'
+
   def install
-    if MacOS.mountain_lion?
-      # Force this formula to use OS X's built-in apr-1-config
-      ENV['HOMEBREW_CCCFG'] = ENV['HOMEBREW_CCCFG'].delete "a"
-      # Force the toolchain reported by apr-1-config
-      ENV['CC'] = "/Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.8.xctoolchain/usr/bin/cc"
-      ENV['CPP'] = ENV['CC'] + " -E"
-      # This formula must be built with the compiler at the same path as `apr-1-config --cc`
-      # (Other workarounds, like HOMEBREW_CCCFG containing 'a', do not seem to work here)
-      unless File.exists?('/Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.8.xctoolchain/usr/bin/cc')
-        abort <<-EOS
-
-ERROR: An OS X bug exists that requires the compiler to be at
-/Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.8.xctoolchain/usr/bin/cc
-
-If you have Xcode installed, create a symbolic link to the correct location:
-ln -s /Applications/Xcode.app/Contents/Developer/Toolchains/{XcodeDefault,OSX10.8}.xctoolchain
-(sudo might be needed if you downloaded Xcode from the App Store)
-
-If you do not have Xcode installed, create a fake OSX10.8.xctoolchain directory pointing to system
-folder that contain the compilers and libraries needed:
-mkdir -p /Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.8.xctoolchain/usr
-ln -s /usr/bin /Applications/Xcode.app/Contents/Developer/Toolchains/OSX10.8.xctoolchain/usr/bin
-EOS
-      end
-    end
     suexec_userdir   = ENV['SUEXEC_USERDIR']  || 'Sites'
     suexec_docroot   = ENV['SUEXEC_DOCROOT']  || '/'
     suexec_uidmin    = ENV['SUEXEC_UIDMIN']   || '500'
     suexec_gidmin    = ENV['SUEXEC_GIDMIN']   || '20'
     suexec_safepath  = ENV['SUEXEC_SAFEPATH'] || '/usr/local/bin:/usr/bin:/bin:/opt/local/bin'
     logfile          = '/private/var/log/apache2/suexec_log'
+
     begin
       suexecbin = `/usr/sbin/apachectl -V`.match(/SUEXEC_BIN="(.+)"/)[1]
     rescue # This should never happen, unless Apple drops support for suexec in the future...
       abort "Could not determine suexec path. Are you sure that Apache has been compiled with suexec support?"
     end
+
     system "./configure",
       "--enable-suexec=shared",
       "--with-suexec-bin=#{suexecbin}",
@@ -52,9 +31,12 @@ EOS
       "--with-suexec-gidmin=#{suexec_gidmin.to_i}",
       "--with-suexec-logfile=#{logfile}",
       "--with-suexec-safepath=#{suexec_safepath}"
+
     system "make"
+
     libexec.install 'modules/generators/.libs/mod_suexec.so'
     libexec.install 'support/suexec'
+
     include.install 'modules/generators/mod_suexec.h'
   end
 

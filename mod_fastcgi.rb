0ee1dd9012f506897ef75ca6f2a559d19576d709
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-26 22:57:47 +0000

    mod_fastcgi: use mirror for url

--- mod_fastcgi.rb
+++ mod_fastcgi.rb
@@ -13,7 +13,9 @@ class ModFastcgi < Formula
 
   desc "Variant of CGI"
   homepage "http://www.fastcgi.com/"
-  url "http://www.fastcgi.com/dist/mod_fastcgi-2.4.6.tar.gz"
+  # Upstream website seems completely dead.
+  # url "http://www.fastcgi.com/dist/mod_fastcgi-2.4.6.tar.gz"
+  url "https://dl.bintray.com/homebrew/mirror/mod_fastcgi-2.4.6.tar.gz"
   sha256 "a5a887eecc8fe13e4cb1cab4d140188a3d2b5e6f337f8a1cce88ca441ddbe689"
 
   option "with-homebrew-httpd22", "Use Homebrew Apache httpd 2.2"
